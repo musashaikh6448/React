00e63b3449931a5593120e4c36b858f13bfb287b
musashaikh6448.bootom-logo{ <shaikhmusa6448@gmail.com>
2024-11-16 13:24:00 +0530

    changes in code

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,28 +1,11 @@
 
-import './App.css'
+// import './App.css'
 
 function App() {
 
   return (
     <>
-   <div id="card">
-    <div className="header-card"></div>
-    <div className="card-body">
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAMjQfOnVjs8CCD4-gLzDvZ0L6npZIUqTd1Q&s"
-        alt="image"
-        className="profile"
-      />
-      <h2>Alaina Wick</h2>
-      <h4>Front-end Developer</h4>
-      <p>
-        Sodales accumsan ligula. Aenean sed diam tristique, fermentum mi nec,
-        ornare arcu.
-      </p>
-      <button>View Profile</button>
-    </div>
-   </div>
-     
+   <h1> hi this react page</h1>
     </>
   )
 }
