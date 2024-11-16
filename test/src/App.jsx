98311ee6770f834d04fe6dbcddb22b31fa5b20ee
musashaikh6448.bootom-logo{ <shaikhmusa6448@gmail.com>
2024-11-16 13:53:05 +0530

    changes in code

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -2,10 +2,12 @@
 // import './App.css'
 
 function App() {
-
+let x=10
+let y=20
   return (
     <>
-   <h1> hi this react page</h1>
+   <h1> hi this react page {x}</h1>
+   <h2>the additon of {x} and  {y} is {x+y}</h2>
     </>
   )
 }
