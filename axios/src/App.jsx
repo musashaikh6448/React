f452529c9d5a794a1bb922779f145919090d20e1
musashaikh6448.bootom-logo{ <shaikhmusa6448@gmail.com>
2024-12-04 15:47:55 +0530

    changes
    git commit -m changes

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
 import Fetch2 from './pages/Fetch2'
-import Movie from './pages/Movie'
+
 const App = () => {
   return (
 <>
-<Movie/>
 <Fetch2/>
 </>  )
 
--- axios/src/pages/Fetch2.jsx
+++ axios/src/pages/Fetch2.jsx
@@ -1,11 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Fetch2 = () => {
-    fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-  return (
-    <div>Fetch2</div>
-  )
-}
+  const [data, setData] = useState([
+    {
+      "id": "",
+      "title": "",
+      "price": 0,
+      "description": "",
+      "category": "",
+      "image": "",
+      "rating": {
+        "rate": "",
+        "count": 0
+      }
+    }
+  ])
 
-export default Fetch2
+  let [category, setCategory] = useState([])
+  useEffect(() => {
+    // fetch("https://fakestoreapi.com/products")
+    //   .then((res) => res.json())
+    //   .then((json) => {
+    //     setData(json); // Wrap the fetched object in an array to keep data consistent
+    //   })
+
+
+      axios .get("https://fakestoreapi.com/products")
+      .then((response) =>{
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        });
+
+
+      axios .get("https://fakestoreapi.com/products/categories")
+      .then((response) =>{
+       setCategory(response.data)
+      })
+      .catch((err) => {
+        console.error(err);
+        });
+    
+  }, []);
+
+  return (
+    <div>
+      {data.map((obj) => {
+        return (
+          <div key={obj.id}>
+            <h2>Title: {obj.title}</h2>
+            <p>Description: {obj.description}</p>
+            <p>Price: ${obj.price}</p>
+            <img src={obj.image} alt={obj.title} height="180px" width="200px" />
+          </div>
+        );
+      })}
+
+      {category.map((ele)=>{
+        return <p key={ele}>{ele}</p>
+      })}
+    </div>
+  );
+};
+
+export default Fetch2;
