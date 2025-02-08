053a8573a3123132c155668b0e51e3a6d7b57739
musashaikh6448.bootom-logo{ <shaikhmusa6448@gmail.com>
2025-02-08 16:55:47 +0530

    new code

--- React2.0/src/App.jsx
+++ React2.0/src/App.jsx
@@ -1,12 +1,34 @@
-import Props from "./Props"
+import Props from "./Props";
 
 const App = () => {
-  return (
-    <div>
-<Props/>
-      
-    </div>
-  )
-}
+  // let name = "shaikh";
+  // let age = 25;
 
-export default App
+  // const student={
+  //   name:"shaikh",
+  //   email:"shaikh8698@gmail.com",
+  //   mobile:123456789990,
+  //   grade:'a+'
+
+  // }
+
+  // const cars = ["suzuki", "toyota", "ford", "honda", "tesla"];
+
+
+  const handleClick=(username)=>{
+    console.log(username);
+  }
+
+  return (
+    <>
+      {/* <Props name={name} age={age} /> */}
+      {/* <Props student={student} />            object */}
+      {/* <Props cars={cars} />                        array */}
+
+<Props  event={handleClick}/>
+
+    </>
+  );
+};
+
+export default App;
--- React2.0/src/Props.jsx
+++ React2.0/src/Props.jsx
@@ -1,8 +1,39 @@
+// const Props = (Props) => {                                             simple
+// const Props = ({ student }) => {                                       object
+// const { name, email, mobile, grade } = student;                       destructing  object
 
-const Props = () => {
+// const Props = ({ cars }) => {                                       //array
+const Props = ({ event }) => {                                   
   return (
-    <div>Props</div>
-  )
-}
+    <>
+      {/* simple */}
+      {/* <h1>{Props.name}</h1>
+    <h1>{Props.age}</h1> */}
 
-export default Props
+      {/* object */}
+      {/* <h1>{name}</h1>
+    <h1>{email}</h1>
+    <h1>{mobile}</h1>
+    <h1>{grade}</h1> */}
+
+      {/* array */}
+      {/* <h1>{cars[0]}</h1>
+<h1>{cars[1]}</h1>
+<h1>{cars[2]}</h1>
+<h1>{cars[3]}</h1>
+<h1>{cars[4]}</h1> */}
+
+      {/* {cars.map((car) => {                                       //array destructing
+        return <h1>{car}</h1>;
+      })} */}
+
+
+{/* function */}
+
+<button className="btn btn-primary" onClick={()=> event('shaikh')}>click me</button>                
+
+    </>
+  );
+};
+
+export default Props;
