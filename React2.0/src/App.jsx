9bd7bfdd3c450fbcb09069613291f515f139ad01
musashaikh6448.bootom-logo{ <shaikhmusa6448@gmail.com>
2025-02-10 15:22:18 +0530

    new file

--- React2.0/src/App.jsx
+++ React2.0/src/App.jsx
@@ -1,34 +1,10 @@
-import Props from "./Props";
 
 const App = () => {
-  // let name = "shaikh";
-  // let age = 25;
-
-  // const student={
-  //   name:"shaikh",
-  //   email:"shaikh8698@gmail.com",
-  //   mobile:123456789990,
-  //   grade:'a+'
-
-  // }
-
-  // const cars = ["suzuki", "toyota", "ford", "honda", "tesla"];
-
-
-  const handleClick=(username)=>{
-    console.log(username);
-  }
-
   return (
     <>
-      {/* <Props name={name} age={age} /> */}
-      {/* <Props student={student} />            object */}
-      {/* <Props cars={cars} />                        array */}
-
-<Props  event={handleClick}/>
-
+    App
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
